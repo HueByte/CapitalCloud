1765937c4c1f39c5d4ee2a8a6c056bd5b3d4c22c
HueByte <ainodeve@gmail.com>
2021-03-30 01:46:14 +0200

    working on menu day 1

--- src/front/huelette/src/components/Menu.jsx
+++ src/front/huelette/src/components/Menu.jsx
@@ -1,20 +1,71 @@
 import React from 'react';
 import './Menu.css';
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.svg';
 
 const Menu = () => {
     return (
-        <section className="top-menu">
-            <div className="desktop">
-                <div className="logo">
-                    I'm logo
+        <section className="nav-top">
+            <div className="nav-desktop">
+                <div className="nav-logo">
+                    <img src={logo} className="triangle" />
+                    {/* <img style={{ width: 250, height: 100 }} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_ZktLhdaw0NzlvRUYAdJHzRnxv8_y9zFLfg&usqp=CAU" /> */}
                 </div>
-                <div className="nav-desktop-container">
-                    <div className="main-nav-desktop-container">
+                <div className="nav-desktop__container">
+                    <div className="nav-desktop__main__container">
                         <div className="nav-left">
-                            I'm s
+                            <Link to="" className="nav-desktop__main__button">
+                                <i class="fa fa-circle-o nav-desktop__main__button-icon"></i>
+                                <div>Wheel</div>
+                            </Link>
+                            <Link to="" className="nav-desktop__main__button">
+                                <i class="fa fa-superpowers nav-desktop__main__button-icon" aria-hidden="true"></i>
+                                <div>Roulette</div>
+                            </Link>
+                            <Link to="" className="nav-desktop__main__button">
+                                <i class="fa fa-area-chart nav-desktop__main__button-icon" aria-hidden="true"></i>
+                                <div>Crash</div>
+                            </Link>
                         </div>
                         <div className="nav-right">
-                            Menu
+                            <div className="nav-coins__container">
+                                <div className="nav-coins-icon">
+                                    <i class="fas fa-coins"></i>
+                                    <span style={{ textTransform: "uppercase", marginLeft: 5 }}>coins</span>
+                                </div>
+                                <div className="nav-coins-balance">
+                                    0
+                                </div>
+                            </div>
+                            <Link to="/profile"
+                                className="nav-avatar"
+                                style={{ backgroundImage: "url(https://lh3.googleusercontent.com/iFjN0aRv7Olsk3uHMzLQdALoJVA3qRyAgJ75Z5PsTLOrUOSzSYP2kbGMvwveZc4a7P9byIV5rbZXDwwfttbyD_wP=w640-h400-e365-rj-sc0x00ffffff)" }}
+                                title="Profile">
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="nav-desktop-sub">
+                        <div className="nav-desktop-sub__left">
+                            <Link to="/" className="nav-desktop-sub-item">
+                                <div className="nav-desktop-sub-item__icon">
+                                    <i class="fa fa-user" aria-hidden="true"></i>   
+                                </div>
+                                <div className="nav-desktop-sub-item__text">My Account</div>
+                            </Link>
+                            <Link to="/" className="nav-desktop-sub-item">
+                                <div className="nav-desktop-sub-item__icon">
+                                    <i class="fa fa-gift" aria-hidden="true"></i>
+                                </div>
+                                <div className="nav-desktop-sub-item__text">Rewards</div>
+                            </Link>
+                            <Link to="/" className="nav-desktop-sub-item">
+                                <div className="nav-desktop-sub-item__icon">
+                                    <i class="fa fa-trophy" aria-hidden="true"></i>
+                                </div>
+                                <div className="nav-desktop-sub-item__text">Leaderboards</div>
+                            </Link>
+                        </div>
+                        <div className="nav-desktop-sub__right">
                         </div>
                     </div>
                 </div>
