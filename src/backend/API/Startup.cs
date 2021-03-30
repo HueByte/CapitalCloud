a2eb880a5d3f0b3a408b8e505cb90c3633358051
OldSinner <antylifepatia@gmail.com>
2021-03-30 18:49:16 +0200

    Base Class for MongoDb

--- src/backend/API/Startup.cs
+++ src/backend/API/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Configuration;
+using Core.RepositoriesInterfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +26,8 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            ModuleConfiguration moduleConfiguration = new ModuleConfiguration(services,Configuration);
+            moduleConfiguration.ConfigureDataBase();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
