06085ccb3f50c5d8bde0eb7357e25a00e94655ce
HueByte <ainodeve@gmail.com>
2021-04-04 00:28:28 +0200

    fix

--- src/backend/API/Configuration/RoleSeed.cs
+++ src/backend/API/Configuration/RoleSeed.cs
@@ -1,26 +1,28 @@
 using System.Threading.Tasks;
 using Core.Entities;
 using Microsoft.AspNetCore.Identity;
+using Common.lib.Types;
 
 namespace API.Configuration
 {
-    public class FirstConfiguration
+    public class RoleSeed
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly RoleManager<ApplicationRole> _roleManager;
-        public FirstConfiguration(UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager)
+        public RoleSeed(UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager)
         {
             _userManager = userManager;
             _roleManager = roleManager;
         }
          public async Task CreateBasicRoles()
         {
-            var resultadmin = await _roleManager.FindByNameAsync("admin");
+            // var resultadmin = await _roleManager.FindByNameAsync("admin");
+            var resultadmin = await _roleManager.FindByNameAsync(Roles.Admin);
             if(resultadmin == null) await _roleManager.CreateAsync(new ApplicationRole()
             {
                 Name = "admin",
             });
-            var resultuser = await _roleManager.FindByNameAsync("user");
+            var resultuser = await _roleManager.FindByNameAsync(Roles.User);
             if(resultuser == null) await _roleManager.CreateAsync(new ApplicationRole()
             {
                 Name = "user",
---
+++ src/backend/Common/lib/Types/Roles.cs
@@ -0,0 +1,8 @@
+namespace Common.lib.Types
+{
+    public static class Roles
+    {
+        public static string Admin { get; set; } = "admin";
+        public static string User { get; set; } = "user";
+    }
+}
