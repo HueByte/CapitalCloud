cce8f3cb6410d24f6316a6248003a8a218b33935
HueByte <ainodeve@gmail.com>
2021-04-05 20:29:33 +0200

    Router with layout and 404

--- src/front/huelette/src/routes/Routes.jsx
+++ src/front/huelette/src/routes/Routes.jsx
@@ -12,32 +12,37 @@ import Register from '../pages/account/register';
 import FOUR_ZERO_FOUR from '../pages/404/FOUR_ZERO_FOUR';
 
 export const Routes = () => {
+    const routes = [
+        {
+            path: "/wheel",
+            component: Wheel
+        }
+    ]
+
     return (
         <>
             <Switch>
                 {/* his can only be used in conjunction with from to exactly match a location 
                 when rendering a <Redirect> inside of a <Switch>. */}
                 <Redirect exact from="/" to="/wheel" />
-                {/* TO CHANGE */}
-                {/* <Route>
-                    <Switch>
-                        <Route path="/account/login" component={Login} />
-                        <Route path="/account/Register" component={Register} />
-                        <Route path="*" component={FOUR_ZERO_FOUR} />
-                    </Switch>
-                </Route> */}
-                <Route path="/account/login" component={Login} />
-                <Route path="/account/Register" component={Register} />
 
-                <BasicLayout>
-                    <Route path="/wheel" component={Wheel} />
-                    <Route path="/roulette" component={Roulette} />
-                    <Route path="/crash" component={Crash} />
-                    <Route exact path="/account/profile" component={Account} />
-                    <Route exact path="/account/rewards" component={Rewards} />
-                    <Route path="/leaderboards" component={Leaderboards} />
-                </BasicLayout>
+                {/* Without layout */}
+                <Route path="/auth/login" component={Login} />
+                <Route path="/auth/Register" component={Register} />
 
+                {/* TODO figure out how to do that other way */}
+                <Route path={["/wheel", "/roulette", "/crash", "/account/profile", "/account/rewards", "/leaderboards"]} component={BasicLayout}>
+                    <BasicLayout>
+                        <Route path="/wheel" component={Wheel} />
+                        <Route path="/roulette" component={Roulette} />
+                        <Route path="/crash" component={Crash} />
+                        <Route exact path="/account/profile" component={Account} />
+                        <Route exact path="/account/rewards" component={Rewards} />
+                        <Route path="/leaderboards" component={Leaderboards} />
+                    </BasicLayout>
+                </Route>
+
+                {/* 404 error */}
                 <Route path="*" component={FOUR_ZERO_FOUR} />
             </Switch>
         </>
