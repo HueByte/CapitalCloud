3a89bc687ee95ecee9c49e82c4eda685726ad40f
HueByte <ainodeve@gmail.com>
2021-04-05 23:57:22 +0200

    UI login designing

--- src/front/huelette/src/pages/auth/login.jsx
+++ src/front/huelette/src/pages/auth/login.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './auth.css';
+import logo from '../../assets/white-cloud.jpg';
 
 const Login = () => {
     return (
@@ -18,6 +19,27 @@ const Login = () => {
                     <div className="auth-buttons__container">
                         <NavLink to="/" className="auth-button">Login</NavLink>
                         <NavLink to="/" className="auth-button">Register</NavLink>
+                    </div>
+                </div>
+            </div>
+
+            <div className="auth__container-desktop">
+                <div className="auth-left-desktop">
+                    {/* TODO make coin drop from img */}
+                    <img src={logo} alt="logo" />
+                </div>
+                <div className="auth-right-desktop">
+                    <div className="auth-right-text">
+                        <span>Welcome to Huelette</span><br />
+                        <span>Log in to enjoy your game!</span>
+                    </div>
+                    <div className="right-input__container">
+                        <input type="text" className="input-desktop" placeholder="E-mail" />
+                        <input type="text" className="input-desktop" placeholder="Password" />
+                    </div>
+                    <div className="right-buttons__container">
+                        <NavLink to="/" className="desktop-button">Home</NavLink>
+                        <NavLink to="/" className="desktop-button">Login</NavLink>
                     </div>
                 </div>
             </div>
