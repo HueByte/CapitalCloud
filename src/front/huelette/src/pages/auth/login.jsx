4103deca77cd1ea50deedf46c9a608cff010172b
HueByte <ainodeve@gmail.com>
2021-04-06 16:34:15 +0200

    new login design

--- src/front/huelette/src/pages/auth/login.jsx
+++ src/front/huelette/src/pages/auth/login.jsx
@@ -6,7 +6,7 @@ import logo from '../../assets/white-cloud.jpg';
 const Login = () => {
     return (
         <div className="auth-wrapper">
-            <div className="auth__container">
+            {/* <div className="auth__container">
                 <div className="auth-home">
                     <NavLink to="/"><i class="fa fa-home" aria-hidden="true"></i></NavLink>
                 </div>
@@ -21,25 +21,30 @@ const Login = () => {
                         <NavLink to="/" className="auth-button">Register</NavLink>
                     </div>
                 </div>
-            </div>
+            </div> */}
 
-            <div className="auth__container-desktop">
-                <div className="auth-left-desktop">
+            <div className="auth__container">
+                <div className="auth-left">
                     {/* TODO make coin drop from img */}
                     <img src={logo} alt="logo" />
                 </div>
-                <div className="auth-right-desktop">
+                <div className="auth-right">
                     <div className="auth-right-text">
                         <span>Welcome to Huelette</span><br />
                         <span>Log in to enjoy your game!</span>
                     </div>
                     <div className="right-input__container">
-                        <input type="text" className="input-desktop" placeholder="E-mail" />
-                        <input type="text" className="input-desktop" placeholder="Password" />
+                        <input type="text" className="auth-input" placeholder="E-mail" />
+                        <input type="text" className="auth-input" placeholder="Password" />
                     </div>
                     <div className="right-buttons__container">
-                        <NavLink to="/" className="desktop-button">Home</NavLink>
-                        <NavLink to="/" className="desktop-button">Login</NavLink>
+                        <div className="buttons-left">
+                            <NavLink to="/" className="auth-problem">Can't log in</NavLink>
+                        </div>
+                        <div className="buttons-right">
+                            <NavLink to="/" className="auth-button">Home</NavLink>
+                            <NavLink to="/" className="auth-button">Login</NavLink>
+                        </div>
                     </div>
                 </div>
             </div>
