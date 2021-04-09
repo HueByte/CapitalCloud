58f3a70ba2b499da2672ac065cf4eccac4d95547
HueByte <ainodeve@gmail.com>
2021-04-09 02:34:07 +0200

    Debug.bat file for faster testing both API and Front

---
+++ debug.bat
@@ -0,0 +1,9 @@
+@echo off
+
+set root=%cd%
+echo %root%
+
+dotnet restore %root%/src/backend
+start "API" cmd /c "dotnet run --project %root%/src/backend/API/API.csproj & pause"
+start "Front" cmd /c "npm start --prefix %root%/src/front/huelette & pause"
+pause
