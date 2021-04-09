c7ff22f65494327e09e7ae5df3cd2fb1f13e9c09
OldSinner <antylifepatia@gmail.com>
2021-04-10 00:52:43 +0200

    fix

--- src/backend/Core/Entities/ApplicationUser.cs
+++ src/backend/Core/Entities/ApplicationUser.cs
@@ -7,9 +7,7 @@ namespace Core.Entities
     public class ApplicationUser : MongoUser<string>
     {
         public string Avatar_Url { get; set; }
-        public int lvl { get; set; } = 1;
-
-        public double exp { get; set; } = 10;
+        public double exp { get; set; } = 1;
         
         
 
