0f4b1e4abe97a091ef23498c6819a26a7069b9f0
HueByte <ainodeve@gmail.com>
2021-04-09 22:28:23 +0200

    Production.bat

---
+++ production.bat
@@ -0,0 +1,14 @@
+@echo off
+mode con: cols=160 lines=78
+set root=%cd%
+
+cd %root%/src/backend/API 
+echo Publishing the API
+dotnet publish -c Release -r win-x64 --output %root%/Deploy/Api
+
+cd %root%/src/front/huelette
+echo Creating front-app build
+start /B /wait "Building Front" cmd /c "npm run build" 
+echo Moving build to %root%/Deploy/Front
+move %cd%/build %root%/Deploy/Front
+pause
