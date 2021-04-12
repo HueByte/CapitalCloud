af50b51a2b7daf3b47ea8c8aa898f71a1bec9254
HueByte <ainodeve@gmail.com>
2021-04-12 16:08:15 +0200

    Production.bat update

--- production.bat
+++ production.bat
@@ -8,6 +8,7 @@ dotnet publish -c Release -r win-x64 --output %root%/Deploy/Api
 
 cd %root%/src/front/huelette
 echo Creating front-app build
+start /B /wait "Building Front" cmd /c "npm update" 
 start /B /wait "Building Front" cmd /c "npm run build" 
 echo Moving build to %root%/Deploy/Front
 move %cd%/build %root%/Deploy/Front
